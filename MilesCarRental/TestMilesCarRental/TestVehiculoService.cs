160aab1f3624a3b423e52b523f72267a4996b6e0
Carlos Mario Rios Yepes <carlos.mario10@hotmail.es>
2024-03-12 23:55:54 -0500

    se agregan pruebas unitarias son mstest

---
+++ MilesCarRental/TestMilesCarRental/TestVehiculoService.cs
@@ -0,0 +1,63 @@
+﻿using MilesCarRental.Application.Services;
+using MilesCarRental.Domain.Contracts;
+using MilesCarRental.Domain.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMilesCarRental
+{
+    [TestClass]
+    public class TestVehiculoService
+    {
+        [TestMethod]
+        public void GetVehiculos_Ok()
+        {
+            //Arrange
+            int idLocalidadRecogida = 1;
+            int idUbicacionCliente = 1;
+            ResultadoBusquedaVehiculos resultadoBusquedaVehiculos = new()
+            {
+                ColorVehiculo = "rojo",
+                PlacaVehiculo = "asd",
+                ModeloVehiculo = "2024",
+                MarcaVehiculo = "mazda"
+            };
+
+            List<ResultadoBusquedaVehiculos> lisResultado = new();
+            lisResultado.Add(resultadoBusquedaVehiculos);
+
+            Mock<IVehiculoRepository> vehiculoRepository = new();
+            VehiculoService servicio = new(vehiculoRepository.Object);
+
+            vehiculoRepository.Setup(a=>a.GetVehiculos(idLocalidadRecogida, idUbicacionCliente)).Returns(lisResultado);
+
+
+            //Act
+            var vehiculos = servicio.GetVehiculos(idLocalidadRecogida, idUbicacionCliente);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, vehiculos.Codigo);
+        }
+
+        [TestMethod]
+        public void GetVehiculos_NoContent()
+        {
+            //Arrange
+            int idLocalidadRecogida = 1;
+            int idUbicacionCliente = 1;
+            Mock<IVehiculoRepository> vehiculoRepository = new Mock<IVehiculoRepository>();
+            VehiculoService servicio = new VehiculoService(vehiculoRepository.Object);
+
+            //Act
+            var vehiculos = servicio.GetVehiculos(idLocalidadRecogida, idUbicacionCliente);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.NoContent, vehiculos.Codigo);
+        }
+    }
+}
